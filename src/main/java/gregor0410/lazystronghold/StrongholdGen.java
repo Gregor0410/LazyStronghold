06d76b2cdb0343258dcf20b4b3e09ed4d157973c
Gregor0410 <34042398+Gregor0410@users.noreply.github.com>
2021-09-06 21:17:31 +0100

    no longer generates strongholds in dimensions that shouldnt have them

--- src/main/java/gregor0410/lazystronghold/StrongholdGen.java
+++ src/main/java/gregor0410/lazystronghold/StrongholdGen.java
@@ -8,16 +8,19 @@ import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.Biomes;
 import net.minecraft.world.biome.source.BiomeSource;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
+import net.minecraft.world.gen.chunk.StrongholdConfig;
 import net.minecraft.world.gen.chunk.StructuresConfig;
+import net.minecraft.world.gen.feature.StructureFeature;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class StrongholdGen implements Runnable {
+    private final StrongholdConfig config;
     private ChunkGenerator generator;
     private Thread thread;
     private Random random;
-    private static final List<Biome> STRONGHOLD_BIOMES = Lists.<Biome>newArrayList(Biomes.PLAINS,Biomes.DESERT,Biomes.MOUNTAINS,Biomes.FOREST,Biomes.TAIGA,Biomes.SNOWY_TUNDRA,Biomes.SNOWY_MOUNTAINS,Biomes.MUSHROOM_FIELDS,Biomes.MUSHROOM_FIELD_SHORE,Biomes.DESERT_HILLS,Biomes.WOODED_HILLS,Biomes.TAIGA_HILLS,Biomes.MOUNTAIN_EDGE,Biomes.JUNGLE,Biomes.JUNGLE_HILLS,Biomes.JUNGLE_EDGE,Biomes.STONE_SHORE,Biomes.BIRCH_FOREST,Biomes.BIRCH_FOREST_HILLS,Biomes.DARK_FOREST,Biomes.SNOWY_TAIGA,Biomes.GIANT_TREE_TAIGA,Biomes.GIANT_TREE_TAIGA_HILLS,Biomes.WOODED_MOUNTAINS,Biomes.BADLANDS_PLATEAU,Biomes.SUNFLOWER_PLAINS,Biomes.DESERT_LAKES,Biomes.GRAVELLY_MOUNTAINS,Biomes.FLOWER_FOREST,Biomes.TAIGA_MOUNTAINS,Biomes.ICE_SPIKES,Biomes.MODIFIED_JUNGLE,Biomes.MODIFIED_JUNGLE_EDGE,Biomes.TALL_BIRCH_FOREST,Biomes.TALL_BIRCH_HILLS,Biomes.DARK_FOREST_HILLS,Biomes.SNOWY_TAIGA_MOUNTAINS,Biomes.GIANT_SPRUCE_TAIGA_HILLS,Biomes.MODIFIED_GRAVELLY_MOUNTAINS,Biomes.SHATTERED_SAVANNA,Biomes.SHATTERED_SAVANNA_PLATEAU,Biomes.ERODED_BADLANDS,Biomes.MODIFIED_WOODED_BADLANDS_PLATEAU,Biomes.MODIFIED_BADLANDS_PLATEAU);
     private int i;
     private int j;
     private int k;
@@ -27,11 +30,14 @@ public class StrongholdGen implements Runnable {
     private int n;
     private BiomeSource biomeSource;
     private long seed;
+    private ArrayList<Biome> list;
+
     public StrongholdGen(ChunkGenerator generator){
         this.generator = generator;
         this.seed = ((ChunkGeneratorAccess)generator).getField_24748();
         this.biomeSource = generator.getBiomeSource();
         this.thread = new Thread(this);
+        this.config = this.generator.getConfig().getStronghold();
     }
     public void start(){
         this.thread.start();
@@ -44,18 +50,24 @@ public class StrongholdGen implements Runnable {
         if(n==0){
             //initialise algorithm
             this.random = new Random(seed);
-            this.i = StructuresConfig.DEFAULT_STRONGHOLD.getDistance();
-            this.j = StructuresConfig.DEFAULT_STRONGHOLD.getCount();
-            this.k = StructuresConfig.DEFAULT_STRONGHOLD.getSpread();
+            this.i = config.getDistance();
+            this.j = config.getCount();
+            this.k = config.getSpread();
             this.d = this.random.nextDouble() * Math.PI * 2.0;
             this.l = 0;
             this.m = 0;
+            this.list = Lists.<Biome>newArrayList();
+            for(Biome biome : this.biomeSource.method_28443()) {
+                if (biome.hasStructureFeature(StructureFeature.STRONGHOLD)) {
+                    list.add(biome);
+                }
+            }
         }
         if(n<j){
             double e = (double)(4 * i + i * m * 6) + (random.nextDouble() - 0.5) * (double)i * 2.5;
             int o = (int)Math.round(Math.cos(d) * e);
             int p = (int)Math.round(Math.sin(d) * e);
-            BlockPos blockPos = this.biomeSource.locateBiome((o << 4) + 8, 0, (p << 4) + 8, 112, STRONGHOLD_BIOMES, random);
+            BlockPos blockPos = this.biomeSource.locateBiome((o << 4) + 8, 0, (p << 4) + 8, 112, list, random);
             if (blockPos != null) {
                 o = blockPos.getX() >> 4;
                 p = blockPos.getZ() >> 4;
--- src/main/java/gregor0410/lazystronghold/mixin/ChunkGeneratorMixin.java
+++ src/main/java/gregor0410/lazystronghold/mixin/ChunkGeneratorMixin.java
@@ -2,19 +2,26 @@ package gregor0410.lazystronghold.mixin;
 
 import gregor0410.lazystronghold.ChunkGeneratorInterface;
 import gregor0410.lazystronghold.StrongholdGen;
+import net.minecraft.world.biome.source.BiomeSource;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
+import net.minecraft.world.gen.chunk.StructuresConfig;
+import net.minecraft.world.gen.feature.StructureFeature;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ChunkGenerator.class)
 public class ChunkGeneratorMixin implements ChunkGeneratorInterface {
+    @Shadow @Final protected BiomeSource biomeSource;
+    @Shadow @Final private StructuresConfig config;
     private StrongholdGen strongholdGen = null;
 
     @Inject(method="method_28509",at=@At("HEAD"),cancellable = true)
     private void genStrongholds(CallbackInfo ci){
-        if(this.strongholdGen==null){
+        if(this.strongholdGen==null&&(this.config.getStronghold()!=null||this.config.getStronghold().getCount()>0)){
             this.strongholdGen = new StrongholdGen((ChunkGenerator)(Object) this);
         }
         ci.cancel();
