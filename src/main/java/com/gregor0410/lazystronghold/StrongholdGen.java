8ee68fd2d0908fb9c34d76fc06e7e44e159ec9cd
contaria <kingcontaria@gmail.com>
2024-11-09 06:13:51 +0100

    optimization: delay biomesource copy until strongholdgen start

--- src/main/java/com/gregor0410/lazystronghold/StrongholdGen.java
+++ src/main/java/com/gregor0410/lazystronghold/StrongholdGen.java
@@ -2,13 +2,11 @@ package com.gregor0410.lazystronghold;
 
 import com.gregor0410.lazystronghold.mixin.ChunkGeneratorAccess;
 import net.minecraft.util.math.ChunkPos;
-import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.source.BiomeSource;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
 import net.minecraft.world.gen.chunk.StrongholdConfig;
 import org.apache.logging.log4j.Level;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -16,9 +14,8 @@ public class StrongholdGen implements Runnable {
     public final StrongholdConfig config;
     private final Thread thread;
     private final ChunkGenerator generator;
-    public final BiomeSource biomeSource;
     private final long seed;
-    private ArrayList<Biome> list;
+    public BiomeSource biomeSource;
     public List<ChunkPos> strongholds;
     public boolean started;
     public final AtomicBoolean completedSignal;
@@ -29,7 +26,6 @@ public class StrongholdGen implements Runnable {
         this.shouldStop = false;
         this.completedSignal = new AtomicBoolean(false);
         this.seed = seed;
-        this.biomeSource = ((ChunkGeneratorAccess) generator).getBiomeSource1().withSeed(seed); //create new biome source instance for thread safety
         this.thread = new Thread(this, "Stronghold thread");
         this.config = generator.getConfig().getStronghold();
         this.strongholds = strongholds;
@@ -38,6 +34,7 @@ public class StrongholdGen implements Runnable {
 
     public void start() {
         this.started = true;
+        this.biomeSource = ((ChunkGeneratorAccess) this.generator).getBiomeSource1().withSeed(this.seed); //create new biome source instance for thread safety
         this.thread.start();
     }
 
